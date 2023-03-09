348de839771ddd3beb56af993fa6670e67974b26
FredDeCeuster <frederik.deceuster@gmail.com>
2023-03-09 15:58:04 +0100

    Added workflows.

---
+++ src/p3droslo/observation.py
@@ -0,0 +1,5 @@
+class Observation:
+    
+    def __init__(self):
+        
+        print('Created an Observation object!')
---
+++ tests/test_observation.py
@@ -0,0 +1,6 @@
+from p3droslo import Observation
+
+
+def test_constructor():
+    
+    obs = Observation()
