46a6621da08e1210e64d7003f7388031b3eb4bfc
FredDeCeuster <frederik.deceuster@gmail.com>
2023-03-27 07:18:05 +0200

    Fix model test.

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,7 @@ def test_constructor():
     
     
     model = TensorModel(
-        var_keys  = ['CO'],
-        box_sizes = (1.0, 1.0, 1.0),
-        box_shape = (100, 100, 100),
+        keys  = ['CO'],
+        sizes = (1.0, 1.0, 1.0),
+        shape = (100, 100, 100),
     )
